c0634bcaf8cf4924bc6f220dd64b0ed578717f71
Prayag866 <145750507+Prayag866@users.noreply.github.com>
2023-09-25 15:55:08 +0530

    1 file modified

--- src/components/Month.tsx
+++ src/components/Month.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -9,71 +9,103 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
-import {Grid} from '@mui/material';
+import { Grid } from '@mui/material';
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
-const MonthYear = ["Aug 2023", "Sept 2023", "Oct 2023"];
-const Year = [2023, 2022,2021];
-const Months = ["January", "Feburary", "March"];
+const MonthYear = ['Aug 2023', 'Sept 2023', 'Oct 2023'];
+const Year = [2023, 2022, 2021];
+const Months = ['January', 'Feburary', 'March'];
 
 const Month = () => {
   return (
     <>
       <br />
-      <Box marginLeft={2} marginRight={6} boxSizing={"border-box"}>
-        <Typography style={{ fontWeight: "bold" }} gutterBottom>Month:</Typography>
-        <Grid container spacing={0} marginLeft={1} marginTop={0.5} >
-
+      <Box marginLeft={2} marginRight={0} boxSizing={'border-box'}>
+        <Typography style={{ fontWeight: 'bold' }} gutterBottom>
+          Month:
+        </Typography>
+        <Grid container spacing={0} marginLeft={1} marginTop={0.5}>
           {/* 3 Months Part  */}
-          <Grid item md={4} xs={12} sm={5} marginBottom={1}  >
-            {/* sx={{ height: 110, border: "2px solid black" }}  */}
-            <Box paddingTop={1} marginRight={1} display={"flex"} flexDirection={"row"}>
+          <Grid item md={4} xs={12} sm={12} marginBottom={1}>
+            <Box
+              paddingTop={1}
+              marginRight={0}
+              display={'flex'}
+              flexDirection={'row'}
+            >
               {MonthYear.map((row) => {
-                return (<>
-                  <Card sx={{ minWidth: 110, marginRight: 1 }}>
-                    <CardMedia
-                      sx={{ height: 30, backgroundColor: "#171D8F" }}
-                    />
-                    <CardContent>
-                      <Typography variant="body2" color="text.secondary"  align="center">
-                        {row}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </>
-                )
+                return (
+                  <>
+                    <Card
+                      sx={{
+                        minWidth: 110,
+                        marginRight: 1,
+                        marginLeft: { sm: '5%' },
+                      }}
+                    >
+                      <CardMedia
+                        sx={{ height: 25, backgroundColor: '#171D8F' }}
+                      />
+                      <CardContent>
+                        <Typography
+                          variant="body2"
+                          color="text.secondary"
+                          align="center"
+                        >
+                          {row}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </>
+                );
               })}
             </Box>
           </Grid>
 
           {/* OR PART */}
-          <Grid item md={1} xs={8} sm={5} marginBottom={1}  >
-            <Box sx={{ height: 80, }} paddingTop={5} borderRadius={2} marginRight={1}>
-              <Typography variant="h6" align="center" top={7} sx={{fontWeight: "bold"}}>OR</Typography>
+          <Grid item md={4} xs={8} sm={7} marginBottom={1}>
+            <Box
+              sx={{ height: 80 }}
+              paddingTop={5}
+              borderRadius={2}
+              marginLefht={2}
+            >
+              <Typography
+                variant="h6"
+                align="center"
+                top={7}
+                sx={{ fontWeight: 'bold' }}
+              >
+                OR
+              </Typography>
             </Box>
           </Grid>
 
-
           {/* Month PART */}
-          <Grid item md={5} xs={12} sm={12} marginBottom={1} marginLeft={4} >
-            <Box sx={{ height: 40 }} paddingTop={0} display={"flex"} flexDirection={"row"} marginRight={1}>
-              <FormControl fullWidth >
+          <Grid item md={4} xs={12} sm={12} marginBottom={1}>
+            <Box
+              sx={{ height: 40 }}
+              paddingTop={3}
+              display={'flex'}
+              flexDirection={'row'}
+              marginRight={0}
+            >
+              <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-label">Year</InputLabel>
                 <Select
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
                   label="Age"
-                  style={{ backgroundColor: "", width: "60%" }}
+                  style={{ backgroundColor: '', width: '80%' }}
                 >
-                    {Year.map((row) => {
-                return (<MenuItem value={row}>{row}</MenuItem>)
-              })}
+                  {Year.map((row) => {
+                    return <MenuItem value={row}>{row}</MenuItem>;
+                  })}
                 </Select>
               </FormControl>
-
 
               <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-label">Month</InputLabel>
@@ -81,11 +113,11 @@ const Month = () => {
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
                   label="Age"
-                  style={{ backgroundColor: "", width: "60%" }}
+                  style={{ backgroundColor: '', width: '90%' }}
                 >
-                    {Months.map((row) => {
-                return (<MenuItem value={row}>{row}</MenuItem>)
-              })}
+                  {Months.map((row) => {
+                    return <MenuItem value={row}>{row}</MenuItem>;
+                  })}
                 </Select>
               </FormControl>
             </Box>
@@ -93,6 +125,6 @@ const Month = () => {
         </Grid>
       </Box>
     </>
-  )
+  );
 };
 export default Month;
