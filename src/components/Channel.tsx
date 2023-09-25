b783378425b234eb4df3f84ae7103c17d3543b40
Prayag866 <145750507+Prayag866@users.noreply.github.com>
2023-09-25 15:38:28 +0530

    1 file modified

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -4,10 +4,17 @@ import { Typography, Button, ButtonBase } from '@mui/material';
 import Box from '@mui/material/Box';
 
 import { Container, Grid } from '@mui/material';
+const cd = { border: '4px dotted black' };
 const Channel = () => {
-  const [open, setOpen] = React.useState(false);
-  const pg = () => {
-    console.log('heiii');
+  const [box1, setBox1] = React.useState(false);
+  const [box2, setBox2] = React.useState(false);
+  const pg = (e) => {
+    setBox1(true);
+    setBox2(false);
+  };
+  const pg1 = (e) => {
+    setBox2(true);
+    setBox1(false);
   };
   return (
     <>
@@ -25,9 +32,12 @@ const Channel = () => {
             marginBottom={1}
             marginRight={'20%'}
             id="Grid1"
+            onClick={pg}
           >
             <Box
-              style={{ height: '20vh', border: '2px solid black' }}
+              style={box1 ? cd : null}
+              border={'2px solid black'}
+              height={'20vh'}
               paddingTop={1}
               borderRadius={4}
               click={pg}
@@ -43,11 +53,21 @@ const Channel = () => {
             </Box>
           </Grid>
 
-          <Grid item md={3} xs={3} sm={3} marginBottom={1} id="Grid2">
+          <Grid
+            item
+            md={3}
+            xs={3}
+            sm={3}
+            marginBottom={1}
+            id="Grid2"
+            onClick={pg1}
+          >
             <Box
-              style={{ height: '20vh', border: '2px solid black' }}
+              style={box2 ? cd : null}
               paddingTop={1}
               borderRadius={4}
+              border={'2px solid black'}
+              height={'20vh'}
             >
               <Typography
                 variant="body2"
